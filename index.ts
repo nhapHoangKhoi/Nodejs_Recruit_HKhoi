dcf9f00c268a0e4fef1aaaf37744e44abc3cdbd9
nhapHoangKhoi <nhaphk@gmail.com>
2025-07-04 07:00:49 +0700

    Test 1

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World 123');
 });
 
 app.listen(port, () => {
