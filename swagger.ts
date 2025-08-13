7a699acd9ca143db3edbd2f4b3f4a4729b8b5ba5
nhapHoangKhoi <nhaphk@gmail.com>
2025-08-13 11:00:27 +0700

    Add swagger ui for api documentation

---
+++ swagger.ts
@@ -0,0 +1,18 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'API documentation'
+  },
+  host: 'localhost:4000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = [
+  // './routes/admin/index.route.ts',
+  './routes/**/index.route.ts',
+];
+
+swaggerAutogen()(outputFile, endpointsFiles, doc);
